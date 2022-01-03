6fe8fc84e374b259fb5764293f0a6c5a6a694eec
jahed.naghipoor <jahed.naghipoor@alexanderthamm.com>
2022-01-03 20:44:03 +0100

    Add main file

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,74 @@
-# Put the code for your API here.
+from fastapi import FastAPI
+from pydantic import BaseModel # pydantic is a python data validation library
+import pandas as pd
+import os
+
+from starter.ml.data import process_data
+from starter.ml.model import inference, load_model
+
+
+categorical_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+
+app = FastAPI() # initialize the application
+
+class Data(BaseModel): 
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
+    
+@app.get("/") # greetings route
+def greetings():
+    return {"Greetings!!!"}
+
+
+@app.post("/inference/") # greetings route
+def inference(data: Data):
+    dict = {
+        "age": data.age,
+        "workclass": data.workclass,
+        "fnlgt": data.fnlgt,
+        "education": data.education,
+        "education-num": data.education_num,
+        "marital-status": data.marital_status,
+        "occupation": data.occupation,
+        "relationship": data.relationship,
+        "race": data.race,
+        "sex": data.sex,
+        "capital-gain": data.capital_gain,
+        "capital-loss": data.capital_loss,
+        "hours-per-week": data.hours_per_week,
+        "native-country": data.native_country
+    }
+
+    data = pd.DataFrame.from_dict(dict)
+
+    encoder = load_model('encoder.pkl')
+    model = load_model('model.pkl')
+    label = load_model('lb.pkl')
+
+    X, _, _, _ = process_data(
+        data, categorical_features=categorical_features, training=False, encoder=encoder
+    )
+    predictions = inference(model, X.reshape(1, 108))
+    predictions = label.inverse_transform(predictions)
+    return predictions[0]
