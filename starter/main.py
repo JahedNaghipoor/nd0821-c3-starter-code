2ac304130acb46aec5320cf7300a45b1e37de00a
jahed.naghipoor <jahed.naghipoor@alexanderthamm.com>
2022-01-05 14:26:09 +0100

    Add aptfile to package

--- starter/main.py
+++ starter/main.py
@@ -6,6 +6,13 @@ import os
 from starter.ml.data import process_data
 from starter.ml.model import inference, load_model
 
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 categorical_features = [
     "workclass",
