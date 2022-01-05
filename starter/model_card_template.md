1d3ec3e667b25eca8935e14f39a8d234e3d60f36
jahed.naghipoor <jahed.naghipoor@alexanderthamm.com>
2022-01-05 20:36:40 +0100

    New tests

--- starter/model_card_template.md
+++ starter/model_card_template.md
@@ -1,17 +1,22 @@
 # Model Card
-
-For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
+This model card describes about the income prediction model trained on census income dataset.
 
 ## Model Details
+The Scikit-Learn's Logistic Regression model is trained on the census income dataset.
 
 ## Intended Use
+This model is created to prove that it's possible to predict a person's income level (>50k, <=50k) based on just a few characteristics of the person including demographics, occupation information.
 
 ## Training Data
+The dataset contains 48842 salary-level instances with an associated person's 14 attributes. 80% are randomly chosen to train a model
 
 ## Evaluation Data
+The dataset contains 48842 salary-level instances with an associated person's 14 attributes. 20% are randomly chosen to test the trained model's performance.
 
 ## Metrics
+The model is evaluated on precision, recall and f1 scores.
 
 ## Ethical Considerations
-
+The model is trained on a public dataset from UCI ML repository.
 ## Caveats and Recommendations
+The model is just created for a demonstration purpose so it's not highly optimized for performance.
--- starter/starter/test_model.py
+++ starter/starter/test_model.py
@@ -1,65 +1,17 @@
-import os
-import pickle
+import numpy as np
+from sklearn.linear_model import LogisticRegression
 
-import pandas as pd
-import pytest
-from sklearn.model_selection import train_test_split
+from ml.model import train_model
 
-from .data import load_data
-from .ml.data import process_data
-from .ml.model import compute_model_metrics, inference, train_model
-
-
-
-data_dir = "starter/data/"
-model_dir = "starter/model/"
-@pytest.fixture 
-def root(): # root directory
-    return os.getcwd()
-
-# @pytest.fixture
-# def files(root):
+def test_train_model():
     
-#     load_path = os.path.join(root, data_dir, "census_cleaned.csv")
-#     data = load_data(load_path)
-#     #data = pd.read_csv("../data/census.csv")
-    
-#     model = os.path.join(root, model_dir, "lr_model.pkl")
-#     with open(model, "rb") as f:
-#         model = pickle.load(f)
+    rf = train_model(np.array([[1.0, 2.0], [2.0, 3.0]]), np.array([[1], [0]]))
 
-#     encoder = os.path.join(root, model_dir, "encoder.pkl")
-#     with open(encoder, "rb") as f:
-#         encoder = pickle.load(f)
+    assert type(rf) == LogisticRegression
 
-#     lb = os.path.join(root, model_dir, "lb.pkl")
-#     with open(lb, "rb") as f:
-#         lb = pickle.load(f)
 
-#     return data, model, encoder, lb
+def test_inference_output_shape():
+    model = LogisticRegression()
+    model.fit(np.array([[1.0, 2.0], [2.0, 3.0]]), np.array([[1], [0]]))
 
-# @pytest.fixture
-# def train_test_data(files):
-#     data, model, encoder, lb = files
-#     train, test = train_test_split(data, test_size=0.20, random_state=42)
-#     return train, test
-
-# def test_train_model(files, root):
-#     data, model, encoder, lb = files
-#     train, test = train_test_split(data, test_size=0.20)
-#     cat_features = [
-#         "workclass",
-#         "education",
-#         "marital-status",
-#         "occupation",
-#         "relationship",
-#         "race",
-#         "sex",
-#         "native-country",
-#     ]
-#     # X_train, y_train, encoder, lb = process_data(
-#     #     train, categorical_features=cat_features, label="salary", training=True
-#     # )
-#     filepath = os.path.join(root, "../model/lr_model.pkl")
-
-#     assert os.path.exists(filepath)
+    assert len(model.predict(np.arange(4).reshape(2, 2))) == 2
