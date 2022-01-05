d92638e84854db67f3da8e57e4a10d35cd106ef1
jahed.naghipoor <jahed.naghipoor@alexanderthamm.com>
2022-01-05 21:08:55 +0100

    Modify some error

--- starter/main.py
+++ starter/main.py
@@ -8,16 +8,14 @@ from pydantic import BaseModel  # pydantic is a python data validation library
 import pandas as pd
 import numpy as np
 
-from starter.ml.data import process_data
-from starter.ml.model import inference, load_model
-
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
-    
-columns = 'age,workclass,fnlgt,education,education-num,marital-status,occupation,relationship,race,sex,capital-gain,capital-loss,hours-per-week,native-country'.replace('-', '_').split(',')
+
+columns = 'age,workclass,fnlgt,education,education-num,marital-status,occupation,relationship,race,sex,capital-gain,capital-loss,hours-per-week,native-country'.replace(
+    '-', '_').split(',')
 
 categorical_features = [
     "workclass",
@@ -29,14 +27,20 @@ categorical_features = [
     "sex",
     "native-country",
 ]
-model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model/lr_model.pkl')
-encoder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model/encoder.pkl')
+model_path = os.path.join(
+    os.path.dirname(
+        os.path.abspath(__file__)),
+    'model/lr_model.pkl')
+encoder_path = os.path.join(
+    os.path.dirname(
+        os.path.abspath(__file__)),
+    'model/encoder.pkl')
 
 with open(model_path, 'rb') as file:
     model = pickle.load(file)
 with open(encoder_path, 'rb') as file:
     encoder = pickle.load(file)
-    
+
 app = FastAPI()  # initialize the application
 
 
@@ -67,10 +71,12 @@ def greetings():
     """
     return "Greetings!!!"
 
+
 @app.post("/predict")
 def predict_salary_level(item: Data):
     item_dict = item.dict()
-    X = pd.DataFrame([[item_dict[column] for column in columns]], columns=columns)
+    X = pd.DataFrame([[item_dict[column]
+                     for column in columns]], columns=columns)
 
     X_categorical = X[categorical_features].values
     X_continuous = X.drop(*[categorical_features], axis=1)
@@ -78,7 +84,6 @@ def predict_salary_level(item: Data):
     X_categorical = encoder.transform(X_categorical)
 
     X = np.concatenate([X_continuous, X_categorical], axis=1)
-
 
     pred = int(model.predict(X)[0])
 
